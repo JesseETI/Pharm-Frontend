238c05cdc43fbdb06924fde8e33b5fc85d5193cb
Roganci Fontelera <60831333+roganci@users.noreply.github.com>
2021-04-03 11:48:21 -0400

    Draft Admin Panel

---
+++ components/admin/dashboard.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div>
+      <div class="rounded-full h-24 w-24 flex items-center justify-center">
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h1>Andhra Maraj</h1>
+        <h2>Admin</h2>
+      </div>
+    </div>
+    <hr />
+    <h3>Pages</h3>
+    <div>
+      <ul class="text-bold flex px-10">
+        <li>
+          <NuxtLink to="/dashboard/" class="block px-10">
+            <div>
+              <div>Home</div>
+              <div><img src="~assets/home_icon.png" alt="home icon" /></div>
+            </div>
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/dashboard/orderList" class="block px-10">
+            <div>
+              <div>Orders</div>
+              <div><img src="~assets/order_icon.png" alt="order icon" /></div>
+            </div>
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/dashboard/inventory" class="block px-10">
+            <div>
+              <div>Inventory</div>
+              <div><img src="~assets/inventory_icon.png" alt="inventory icon" /></div>
+            </div>
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/dashboard/customerList" class="block px-10">
+            <div>
+              <div>Customers</div>
+              <div><img src="~assets/customer_icon.png" alt="customer icon" /></div>
+            </div>
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/dashboard/activity" class="block px-10">
+            <div>
+              <div>Activity Log</div>
+              <div><img src="~assets/history_icon.png" alt="history icon" /></div>
+            </div>
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+}
+</script>
+
+<style></style>
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <AdminHeader />
+    <div>
+      <NuxtLink to="/dashboard/orderList">
+        <div>
+          <div class="h-200px w-200px"><img src="~assets/box.png" alt="order"  /></div>
+          <div>Manage orders</div>
+        </div>
+      </NuxtLink>
+      <NuxtLink to="/dashboard/inventory">
+        <div>
+          <div class="h-193px w-193px"><img src="~assets/inventory.png" alt="inventory" /></div>
+          <div>Manage Inventory</div>
+        </div>
+      </NuxtLink>
+      <NuxtLink to="/dashboard/customerList">
+        <div>
+          <div class="h-160px w-231px" ><img src="~assets/customer.png" alt="customer"/></div>
+          <div>List of Customers</div>
+        </div>
+      </NuxtLink>
+      <NuxtLink to="/dashboard/activity">
+        <div>
+          <div class="h-170px w-170px"><img src="~assets/history.png" alt="activity"/></div>
+          <div>Admin Activity</div>
+        </div>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
